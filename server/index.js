95cd80be7e9f190143b19296f34bb2eeb5c62c03
itsjustkevin <kevinjchisholm@google.com>
2022-05-20 15:49:53 -0500

    working base

--- server/index.js
+++ server/index.js
@@ -9,21 +9,26 @@ const db = getFirestore(firebaseApp);
 
 // express setup
 const app = express()
-app.use(express.urlencoded({extended: true}))
-app.use(express.json)
+// app.use(express.urlencoded({extended: true}))
+// app.use(express.json)
 const PORT = 3000
 
 app.get('/', (req, res) => {
     res.send('Welcome to tech squad')
 })
 
-// app.get('/test', async (req, res) => {
-//   // const querySnapshot = await getDocs(collection(db, "test"));
-//   // querySnapshot.forEach((doc) => {
-//   //   console.log(`${doc.id} => ${doc.data().text}`);
-//   // });
-//   // res.send(doc)
-// })
+app.get('/test', async (req, res) => {
+  const querySnapshot = await getDocs(collection(db, "test"));
+  const documents = []
+
+  querySnapshot.forEach((doc) => {
+    // test logs
+    console.log(`${doc.id} => ${doc.data().text}`);
+    documents.push(doc.data())
+  });
+  res.send(documents)
+})
+
 app.listen(PORT, () => {
     console.log(`listening on port: ${PORT}`);
 })
