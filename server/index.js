3128a81165200e6428716bc25606ca185354f0df
Ricardo <35527795+ricardo-gonzalez-villegas@users.noreply.github.com>
2022-05-31 15:11:31 -0400

    created seperate routing  (#14)
    * extracted routing logic
    
    * seperated routing
    
    Co-authored-by: Kevin Chisholm <kevinjchisholm@gmail.com>

--- server/index.js
+++ server/index.js
@@ -1,39 +1,17 @@
 import express from 'express'
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
-import { firebaseConfig } from './config.js';
 import { stepRouter } from './routes/step.js';
-// import * as stepRoute from './routes/step'
-// import {getAll} from './routes/step' 
-
-// firebase config
-const firebaseApp = initializeApp(firebaseConfig);
-const db = getFirestore(firebaseApp);
+import { dataRouter } from './routes/data.js';
 
 // express setup
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.use('/step', stepRouter)
+app.use('/data', dataRouter)
 
 app.get('/', (req, res) => {
     res.send('Welcome to tech squad')
     console.log(process.env)
-})
-
-
-// [TODO Ricardo] Extract to separate route file and make this method functional
-// should not be mutating the array.
-app.get('/test', async (req, res) => {
-  const querySnapshot = await getDocs(collection(db, "test"));
-  const documents = []
-
-  querySnapshot.forEach((doc) => {
-    // test logs
-    console.log(`${doc.id} => ${doc.data().text}`);
-    documents.push(doc.data())
-  });
-  res.send(documents)
 })
 
 // [TODO Ricardo] make this work independent of if we are on local or remote hosting.
