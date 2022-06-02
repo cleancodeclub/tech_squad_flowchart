0b0741e86e9b16416df90a51c1bdb3b1875f60f7
Ricardo Gonzalez <rikarudogonzarezu@gmail.com>
2022-06-02 18:15:08 -0400

    created route to grab single document

--- server/routes/data.js
+++ server/routes/data.js
@@ -1,16 +1,18 @@
 import express from "express";
-import { initializeApp } from "firebase/app";
+import { initializeApp} from "firebase/app";
 import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
 import { firebaseConfig } from "../config.js";
 export const dataRouter = express.Router();
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc} from "firebase/firestore";
+import { async } from "@firebase/util";
 
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
+const targetId = 'GyD5uxvecyXcayraC5ds'
 
 //returns all data
 dataRouter.get("/", async (req, res) => {
-  const querySnapshot = await getDocs(collection(db, "test"));
+  const querySnapshot = await getDocs(collection(db, "test"))
   const documents = querySnapshot.docs;
   const documentCopy = documents.map((doc) => {
     return {
@@ -22,11 +24,24 @@ dataRouter.get("/", async (req, res) => {
   res.send(documentCopy);
 });
 
+// return one document
 dataRouter.get("/:id", async (req, res) => {
-  // const id = await req.params.id
+  const dataCollection = await collection(db, "test")
+  const dataSnapshot = await getDocs(dataCollection)
+  const dataSource = dataSnapshot.docs.map((doc) => {
+    return {
+      id: doc.id,
+      data: doc.data().text,
+      imgURL: doc.data()?.imgURL,
+    };
+  })
+  const document = dataSource.filter(doc => doc.id === targetId)
+  
+  res.send(document)
+})
 
-  //   const snapshot = await db.collection('test').doc(id).get();
-  //   const data = snapshot.data();
-  //   console.log(`data: ${data}`)
-  //   res.send(data)
-});
+
+
+
+
+
