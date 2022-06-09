f107c40e30cb28afa028094965d6f0b040da8b1e
itsjustkevin <kevinjchisholm@google.com>
2022-06-09 14:12:08 -0500

    testing all data functinality

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,31 @@
 <template>
   <navbar></navbar>
   <p>Hello Tech Squad</p>
+  <button @click="getAllData">Get All Data</button>
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue";
+import axios from 'axios'
 
 export default {
   components: {
     'navbar': Navbar
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    getAllData() {
+      axios.get('http://localhost:8080/data')
+        .then(response => {
+          console.log(response);
+        })
+    }
   }
+
 }
 </script>
 
