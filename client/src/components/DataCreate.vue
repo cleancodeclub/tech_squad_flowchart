fbb44bbe4358eb3c3a4e1ca5b0181294045b8c2a
itsjustkevin <kevinjchisholm@google.com>
2022-06-15 14:01:18 -0500

    basic form skeleton

---
+++ client/src/components/DataCreate.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+    <div>
+      <form @submit.preventDefault>
+        <div>
+          <label for="text">Text: </label>
+          <textarea id="text"
+                    rows="5"
+                    v-model="text"></textarea>
+        </div>
+      </form>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: dataCreate,
+  components: [],
+  data() {
+    return {
+      text: '',
+      imageURL: ''
+    }
+  },
+  methods: {}
+}
+</script>
