1459c276fa6ae17daab16f526d15db1be635fb49
itsjustkevin <kevinjchisholm@google.com>
2022-06-27 14:07:47 -0500

    frontend updates

--- client/src/views/CreateView.vue
+++ client/src/views/CreateView.vue
@@ -21,14 +21,18 @@
                      drop-placeholder="Drop file here..."></b-form-file>
       </b-form-group>
       <b-form-group class="ml-auto">
-        <b-button variant="primary">Submit</b-button>
-        <b-button variant="warning">Reset</b-button>
+        <b-button variant="primary"
+                  @click="onSubmit">Submit</b-button>
+        <b-button variant="warning"
+                  @click="onReset">Reset</b-button>
       </b-form-group>
     </b-form>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data() {
     return {
@@ -39,9 +43,14 @@ export default {
     }
   },
   methods: {
-    onSubmit(event) {
+    async onSubmit(event) {
+      const formData = new FormData()
+      formData.append('text', this.form.text)
+      formData.append('image', this.form.image)
       event.preventDefault()
-      alert(JSON.stringify(this.form))
+      // alert(JSON.stringify(formData))
+      await axios.postForm('http://localhost:8080/data/upload', formData)
+        .then(response => console.log(response))
     },
     onReset(event) {
       event.preventDefault()
